00ce497bdb18599b8d82ba60846e048111f16025
Craig Peacock <craigp@beyondlogic.org>
2022-01-19 16:50:47 -0800

    Initial project structure

---
+++ main/main.c
@@ -0,0 +1,31 @@
+/*
+ESP32 EVSE Demo Code
+IEC61851/SAE J1772 
+
+Copyright (C) 2022 Craig Peacock
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+02110-1301, USA.
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+
+void app_main(void)
+{
+
+}
